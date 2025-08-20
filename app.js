bd6cc987bf8e73d97d052342a56a24ea85fdd42c
Ranimarif1 <ranimarif93@gmail.com>
2025-08-21 00:25:46 +0200

    routes config

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const { connectMongoDB } = require("./config/db");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+const osRouter = require("./routes/osRouter"); // chemin correct
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/os", osRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
